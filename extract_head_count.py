09f44a5225c138349d39a6ac1d959def8dc0fae1
JGorman1 <gormanj117@gmail.com>
2023-01-19 18:33:10 -0500

    update code

--- extract_head_count.py
+++ extract_head_count.py
@@ -1,10 +1,3 @@
-"""
-Put data in a dictionary
-when job completes, convert dictionary
-to pandas datafrae
-
-timeout
-"""
 
 import requests
 import re
@@ -13,77 +6,117 @@ import lxml
 import logging
 import datetime
 import pytz
+import mysql
+from requests.adapters import HTTPAdapter
 
 
 class ExtractHeadCount:
 
-    def __init__(self, site_url):
+    MAX_RETRIES_PER_REQUEST = 3
+    REQUEST_TIMEOUT = 10
+
+    def __init__(self, URL : str, upload_location : str):
         self.current_time = self._get_current_time()
-        self.site_url = site_url
+        self.URL = URL
+        self.upload_location = upload_location
+        self.session = requests.Session()
+        self.session.mount("https://display.safespace.io/", 
+                        adapter=HTTPAdapter(max_retries=self.MAX_RETRIES_PER_REQUEST))
 
     def _get_current_time(self):
         """
-        :returns: the current timestamp objects
-        that is utc timezone aware.
+        :return: the current timestamp objects that is utc timezone aware.
         """
         return datetime.datetime.now(tz=pytz.utc)
 
-    def get_response(self, link : str, parser='lxml'):
+    def get_response(self, URL : str, parser='lxml'):
         """
         :param link: link for website being queried
         :param parser:  html parser, default lxml
-        :returns: BeautifulSoup object with HTML lines
+        :return: BeautifulSoup object with HTML lines
         """
         timestamp_formatted = self.current_time.iso_format()
 
         try:
-            response_soup = None
-            #Get a response to confirm the page is working
-            response = requests.get(link, timeout=5)
+            response = self.session.get(URL, timeout=self.REQUEST_TIMEOUT)
             response.raise_for_status()
-            response_soup = BeautifulSoup(response)
         except requests.Timeout:
-            logging.debug(f"ERROR: timeout limit reached for response \
+            logging.error(f"ERROR: timeout limit reached for response \
                 at time: {timestamp_formatted}")
         except requests.HTTPError:
-            logging.debug(f"ERROR: status code {response.status_code} \
-                raised error for response {timestamp_formatted}")
+            logging.error(f"ERROR: status code {response.status_code} \
+                resulted in error for response {timestamp_formatted}")
         except Exception as e:
-            logging.debug(f"ERROR: an exception was raised of type '{e}' \
+            logging.error(f"ERROR: an exception resulted of type '{e}' \
                 for response: {timestamp_formatted}")
+        else:
+            return BeautifulSoup(response, parser)
+        
+        return None
 
-        if not response_soup:
-            return None
-
-        return response_soup
 
     def extract_data(self, response : str):
         """
         :param response: html string
-        :returns: the desired data from the webpage
+        :return: the desired data from the webpage
         """
-        if not response:
-            return None
-        
         head_count_pattern = re.compile(r'<body>(\d+)<body>')
         head_count = re.search(head_count_pattern, str(response)).group(1)
 
         if not head_count:
-            logging.debug(f"NO COUNT: the head_count_pattern did not \
-            match any number.")
-            head_count = None
+            logging.warn(f"NO COUNT: the head_count_pattern regex did not \
+                match any number.")
+        else:
+            head_count = int(head_count)
 
         return head_count
 
-    def main(self):
-        response = self.get_response(self.site_url)
+    def upload_data(self, location : str, head_count : str):
+        """
+        :param location: SQL database upload location
+        :param head_count: head_count int
+        """
+        database = ExtractHeadCount.mysql_connect()
+        sql = """INSERT INTO vital_head_count (head_count, time) VALUES (%s, %s)"""
+        vals = (head_count, self.current_time)
+        logging.debug(f"Entering the following into vital_head_count table: \
+            {head_count}, {self.current_time}")
+        database.cursor.execute(sql, vals)
+        database.commit()
+
+    def extract_upload(self):
+        """
+        :return:
+        """
+        response = self.get_response(self.URL)
         head_count = self.extract_data(response)
+        self.upload_data(self.upload_location, head_count)
 
         return head_count
+    
+    @staticmethod
+    def mysql_connect(host : str, user : str, passwd : str, database : str, retry=0):
+        try:
+            db_connection = mysql.connector.connet(
+                host=host,
+                user=user,
+                passwd=passwd,
+                database=database
+            )
+        except Exception as e:
+            logging.error(f"ERROR: An exception of type {e} resulted while \
+                uploading to database")
+            if retry == 3:
+                raise
+            ExtractHeadCount.mysql_connect(host, user, passwd, database, retry=retry+1)
+        else:
+            logging.debug(f"CONNECTION TO: {host} ESTABLISHED")
+            return db_connection
 
 
 if __name__ == "__main__":
     SITE_URL = "https://display.safespace.io/value/live/a7796f34"
-    ExtractHeadCount(SITE_URL).main()
+    UPLOAD_LOCATION = ""
+    ExtractHeadCount(SITE_URL, UPLOAD_LOCATION).extract_upload()
             
 
